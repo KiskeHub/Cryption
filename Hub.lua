26a955b67e5be079197225622f97b221ce42810e
Cryption <124207931+CryptionHub@users.noreply.github.com>
2023-02-05 01:12:48 +0100

    Update Hub.lua

--- Hub.lua
+++ Hub.lua
@@ -1,6 +1,6 @@
 if game.PlaceId == 3978370137 then 
-    loadstring(game:HttpGet('https://raw.githubusercontent.com/CryptionHub/Cryption/main/FreeGpoMain.lua'))()
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/CryptionHub/Cryption/main/GpoMain.lua'))()
 end
-if game.PlaceId == 9999 then 
-    loadstring(game:HttpGet('https://raw.githubusercontent.com/CryptionHub/Cryption/main/FreeBrGpo.lua'))()
+if game.PlaceId == 11424731604 then 
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/CryptionHub/Cryption/main/BrGpo.lua'))()
 end
